50d9a58b89afe6a5e5cf92cdff580b45ed6de40a
victhangnguyen <victhangnguyen@outlook.com>
2024-12-14 00:24:50 +0700

    init project, add assets-GameRes-sprites.

---
+++ assets/Scripts/Hero.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Hero')
+export class Hero extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
